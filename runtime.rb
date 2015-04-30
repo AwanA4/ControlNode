04d0e902b06d374efef0e57e360e0addfff3bc83
I Putu Pradnyana <pradnyana12@mhs.if.its.ac.id>
2015-04-30 15:44:14 +0000

    Add checking if process is running in ComputeNode

--- runtime.rb
+++ runtime.rb
@@ -8,11 +8,16 @@ require 'net/http'
 require 'thread'
 
 configure{ set :server, :puma}
+configure{ :development}
+configure{ enable :logging, :dump_errors, :raise_errors}
 
 $process_hash = Hash.new
 $Process = Struct.new(:user, :cpu, :ram, :repo, :container, :ip_addr)
+$all_output = ''
 $Template = LXC::Container.new('ComputeNode')
-
+$thread_list = Array.new
+$started = false
+$process_queue = Queue.new
 def randomChar(length)
 	o = [('a'..'z'), ('A'..'Z')].map{|i| i.to_a}.flatten
 	string = (0..length).map{ o[rand(o.length)]}.join
@@ -20,17 +25,30 @@ def randomChar(length)
 end
 
 def watch_process(info)
-	process = LXC::Container.new(info[:container])
-	ip_addr = info.ip_addr
+	ip_addr = info.ip_addr[0]
 	http = Net::HTTP.new(ip_addr, '80')
 	check_alive = Net::HTTP::Get.new('/status')
 	usage_request = Net::HTTP::Get.new('/usage')
 
+	process = LXC::Container.new(info[:container])
+	while process.ip_addresses[0].nil?
+		sleep 1
+	end
+	process.attach(wait: true) do
+		begin
+			output = LXC.run_command('/etc/init.d/compute status')
+			puts info[:container] + ' ' + output
+			sleep 1
+		end until output == 'Running'
+	end
 	repo_post = Net::HTTP::Post.new('/repo')
-	repo_post.body = {'repo' => info[:repo]}.to_json
-	http.request(repo_post)
+	repo_post.body = {'repo' => info['repo'].to_s}.to_json
+	puts info[:container] + " send repo " + info[:repo]
+	response = http.request(repo_post)
 	start_get = Net::HTTP::Get.new('/start')
-	http.request(start_get)
+	response = http.request(start_get)
+	puts info[:container] + " ssend start"
+	$started = true
 	begin
 		response = http.request(check_alive)
 		parsed_response = JSON.parse(response.body.read)
@@ -41,15 +59,17 @@ def watch_process(info)
 		#Sleep for fixed time
 		sleep(20)
 	end until parsed_response['finished']
-	output_request = = Net::HTTP::Get.new('/all_output')
+	output_request = Net::HTTP::Get.new('/all_output')
 	response = http.request(output_request)
 	parsed_response = JSON.parse(response.body.read)
+	$all_output = parsed_response
 	#Put output to database
 
 	#
 	#Remove process from hash
 	$process_hash.delete(info[:container])
-
+	$thread_list.delete(Thread.current)
+	process = LXC::Container.new(info['container'])
 	process.stop
 	process.destroy
 end
@@ -60,6 +80,15 @@ before do
 	@req_data = JSON.parse request.body.read
 end
 
+Thread.new do
+	while true do
+		info = $process_queue.pop
+		puts "Container " + info[:container] + " started"
+		$process_hash.store(info[:container], info)
+		$thread_list << Thread.new{watch_process(info)}
+	end
+end
+
 post '/container' do
 	#Sent data => user, cpu, ram, repo
 	unless @req_data['user'].nil? or @req_data['ram'].nil? or @req_data['repo'].nil?
@@ -67,26 +96,32 @@ post '/container' do
 		new_container = $Template.clone(container_name, {:flags => LXC::LXC_CLONE_SNAPSHOT})
 
 		#start the process
-		new_container.set_cgroup_item('memory.limit_in_bytes', @req_data['ram'].to_s)
+		#new_container.set_cgroup_item('memory.limit_in_bytes', @req_data['ram'].to_s)
+		new_container.save_config
 		new_container.start({:daemonize => true})
-		new_process = $Process.new(@req_data['user'], @req_data['cpu'], @req_data['ram'], @req_data['repo'], container_name, new_container.ip_address)
-		$process_hash.store(container_name, new_process)
+		new_process = $Process.new(@req_data['user'], @req_data['cpu'], @req_data['ram'], @req_data['repo'], container_name, new_container.ip_addresses)
+		#$process_hash.store(container_name, new_process)
 		#Start watch_process thread
-		Thread.new{watch_process(new_process)}
+		$process_queue.push(new_process)
+		#$thread_list << Thread.new{watch_process(new_process)}
+		body(container_name)
 		redirect ('/container/' + container_name + '/info')
 	end
 end
 
 get '/container/:id/output' do
-	if not $process_hash[params[:id]].nil?
+	if not $process_hash[params['id']].nil?
 		info = $process_hash[params[:id]]
-		ip_addr = info[:ip_addr]
+		ip_addr = info[:ip_addr][0]
 		http = Net::HTTP.new(ip_addr, '80')
 		req = Net::HTTP::Get.new('/output')
 		response = http.request(req)
 
 		content_type :json
-		response.body.read
+		response.body unless response.nil?
+	else
+		content_type :json
+		$all_output.to_json unless $all_output.nil?
 	end
 end
 
@@ -94,7 +129,7 @@ post '/container/:id/input' do
 	input = @req_data['stdin']
 	if not $process_hash[params[:id]].nil?
 		info = $process_hash[params[:id]]
-		http = Net::HTTP.new(info[:ip_addr], '80')
+		http = Net::HTTP.new(info[:ip_addr][0], '80')
 		req = Net::HTTP::Post.new('/input')
 		req.body = {'stdin' => input}.to_json
 		http.request(req)
@@ -110,7 +145,9 @@ get '/container/:id/info' do
    'user' => info[:user],
 		'repo' => info[:repo],
 		'cpu' => info[:cpu],
-		'ram' => info[:ram]
+		'ram' => info[:ram],
+		'ip' => info[:ip_addr][0].to_s,
+		'started' => $started
 		}.to_json
 	end
 end
